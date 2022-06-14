57244bf180252ad1b013528f98c4f8777eb62ab5
Fernando Predes <fernando_predes@hotmail.com>
2022-06-14 00:23:15 -0300

    initial pundit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-
+  before_action :authenticate_user!
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:comment))
@@ -8,7 +8,7 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to post_path(@post)
     else
-      render 'new'
+      render :new
     end
   end
 
