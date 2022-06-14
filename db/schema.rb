d8a9c3f6379178efb012ec94e917c8276a5f9fdb
Fernando Predes <fernando_predes@hotmail.com>
2022-06-13 22:47:29 -0300

    created comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_13_205317) do
+ActiveRecord::Schema.define(version: 2022_06_14_011413) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -32,4 +42,6 @@ ActiveRecord::Schema.define(version: 2022_06_13_205317) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
 end
