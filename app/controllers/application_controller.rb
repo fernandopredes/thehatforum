27c3268392db942a70f6672c071f258e92addb01
Fabrizio Saullo <fabriziodsaullo@gmail.com>
2022-06-14 14:49:21 -0300

    fix bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[username])
